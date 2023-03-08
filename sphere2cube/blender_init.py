33fe682200cd4126433c764d6291ec273d255229
Ujjwal Nepal <nepalujjwal95@gmail.com>
2023-03-08 21:01:19 +0545

    Fixed the issue with pink image

--- sphere2cube/blender_init.py
+++ sphere2cube/blender_init.py
@@ -3,7 +3,8 @@ __author__ = 'Xyene'
 import bpy
 import sys
 
-bpy.data.textures[0].image = bpy.data.images.load("%s" % sys.argv[-6])
+# bpy.data.textures[0].image = bpy.data.images.load("%s" % sys.argv[-6])
+bpy.data.images['test.jpg'].filepath = "%s" % sys.argv[-6]
 bpy.context.scene.render.resolution_x = bpy.context.scene.render.resolution_y = int(sys.argv[-5])
 
 sphere = bpy.data.objects["Sphere"]
